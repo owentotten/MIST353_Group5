5a0158851822223ba2afbb62c3f3306cf4bb7354
owentotten <146154955+owentotten@users.noreply.github.com>
2026-02-17 20:47:45 -0500

    Created structure for database

---
+++ CreateTable_Group5.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE Group5;
+GO
+USE Group5;
+GO
+
+create table USER (UID int not null primary key, UFirstName varchar(255) not null, ULastName varchar(255) not null, UEmail varchar(255) not null, UPhone VARCHAR(255) not null);
